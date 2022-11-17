db9aeb69d4980bcf91828e7fca94fe550cddadfd
Юлия Волкова <volktul@gmail.com>
2022-11-17 20:29:30 +0300

    Добавьте файлы проекта.

---
+++ SR11/LinearFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR11
+{
+    struct LinearFunction
+    {
+        double k;
+        double b;
+
+        public LinearFunction(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+
+        }
+        public string Root()
+        {
+            double x = -b / k;
+            return x.ToString();
+
+        }
+    }
+}
---
+++ SR11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SR11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите коэффициент k");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LinearFunction linearFunction = new LinearFunction(k, b);
+            Console.WriteLine(linearFunction.Root());
+            Console.ReadKey();
+        }
+    }
+}
